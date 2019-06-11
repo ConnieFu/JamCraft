9f9a4e4239e701a511d8d8269780d5f8752434a1
TofuTart <connie.fu.888@gmail.com>
2019-06-11 18:44:05 -0400

    I forgot to commit....good thing i'm working alone haha
    + Game Flow base integration
    + Started tileset assets
    + Basic Character Movement

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private const float LERP_VALUE = 0.8f;
+
+    private Vector3 m_Input = Vector3.zero;
+
+    [SerializeField] private float m_Speed = 500.0f;
+    [SerializeField] private Rigidbody2D m_CharacterRigidBody;
+
+    void FixedUpdate()
+    {
+        m_Input.x = Mathf.Lerp(0, Input.GetAxis("Horizontal") * m_Speed, LERP_VALUE);
+        m_Input.y = Mathf.Lerp(0, Input.GetAxis("Vertical") * m_Speed, LERP_VALUE);
+
+        m_CharacterRigidBody.velocity = m_Input;
+    }
+}
---
+++ Assets/Scripts/GameConstants.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameConstants
+{
+    public const string MAIN_MENU_SCENE_NAME = "MainMenu";
+    public const string PAUSE_MENU_SCENE_NAME = "PauseMenu";
+    public const string GAME_OVER_MENU_SCENE_NAME = "GameOverMenu";
+}
