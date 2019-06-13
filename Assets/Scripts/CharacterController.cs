f0477d09d5c32c163ad5aa676daee474fdb750cc
TofuTart <connie.fu.888@gmail.com>
2019-06-13 02:25:56 -0400

    Did a few things here and there huzzah
    + Added proper ground tile assets
    + Added tile highlighting and selection
    + Pause functionality

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,45 +1,109 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CharacterController : MonoBehaviour
 {
     private const float LERP_VALUE = 0.8f;
+    private const float HIGHLIGHT_Z_DEPTH = 0.1f;
 
-    private Vector3 m_Input = Vector3.zero;
-
+    [Header("Movement")]
     [SerializeField] private float m_Speed = 20.0f;
     [SerializeField] private Rigidbody2D m_CharacterRigidBody;
+    private Vector3 m_Input = Vector3.zero;
+
+    [Header("Animations")]
     [SerializeField] private Animator m_CharacterAnimator;
     [SerializeField] private SpriteRenderer m_CharacterSprite;
+    private Vector3Int m_FacingDirection = Vector3Int.down;
 
-    void FixedUpdate()
+    [Header("")]
+    [SerializeField] private Grid m_Grid;
+    [SerializeField] private SpriteRenderer m_TileHighlight;
+    private List<Tilemap> m_TileMaps = new List<Tilemap>();
+    private List<TileBase> m_SelectedTiles = new List<TileBase>();
+    private Vector3Int m_CurrentTilePos;
+    private Vector3 m_CurrentHighlightPos;
+
+    public void Initialize()
     {
-        m_Input.x = Mathf.Lerp(0, Input.GetAxis("Horizontal") * m_Speed, LERP_VALUE);
-        m_Input.y = Mathf.Lerp(0, Input.GetAxis("Vertical") * m_Speed, LERP_VALUE);
-
-        m_CharacterAnimator.SetBool("IsMoving", m_Input.magnitude > 0.0f);
-
-        if (m_Input.y > 0)
+        Tilemap[] tilemaps = m_Grid.GetComponentsInChildren<Tilemap>();
+        foreach (Tilemap tilemap in tilemaps)
         {
-            m_CharacterAnimator.SetInteger("Direction", 1);
-        }
-        else if (m_Input.y < 0)
-        {
-            m_CharacterAnimator.SetInteger("Direction", 0);
-        }
-        else if (m_Input.x > 0)
-        {
-            m_CharacterAnimator.SetInteger("Direction", 2);
-            m_CharacterSprite.flipX = false;
-        }
-        else if (m_Input.x < 0)
-        {
-            m_CharacterAnimator.SetInteger("Direction", 2);
-            m_CharacterSprite.flipX = true;
+            m_TileMaps.Add(tilemap);
         }
 
-        m_CharacterRigidBody.velocity = m_Input;
+        transform.position = Vector3.zero;
+        gameObject.SetActive(true);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!GameFlow.s_IsPaused)
+        {
+            m_Input.x = Mathf.Lerp(0, Input.GetAxis("Horizontal") * m_Speed, LERP_VALUE);
+            m_Input.y = Mathf.Lerp(0, Input.GetAxis("Vertical") * m_Speed, LERP_VALUE);
+
+            m_CharacterAnimator.SetBool("IsMoving", m_Input.magnitude > 0.0f);
+
+            if (m_Input.y > 0)
+            {
+                m_FacingDirection = Vector3Int.up;
+                m_CharacterAnimator.SetInteger("Direction", 1);
+            }
+            else if (m_Input.y < 0)
+            {
+                m_FacingDirection = Vector3Int.down;
+                m_CharacterAnimator.SetInteger("Direction", 0);
+            }
+            else if (m_Input.x > 0)
+            {
+                m_FacingDirection = Vector3Int.right;
+                m_CharacterAnimator.SetInteger("Direction", 2);
+                m_CharacterSprite.flipX = false;
+            }
+            else if (m_Input.x < 0)
+            {
+                m_FacingDirection = Vector3Int.left;
+                m_CharacterAnimator.SetInteger("Direction", 2);
+                m_CharacterSprite.flipX = true;
+            }
+
+            m_CharacterRigidBody.velocity = m_Input;
+
+            // Handle Highlighting Tile in front of Character
+            SetHighlightedTile();
+        }
+    }
+
+    // gets list of tiles from tilemaps at the position of the character
+    private void SetHighlightedTile()
+    {
+        if (m_TileMaps != null && m_TileMaps.Count > 0)
+        {
+            m_SelectedTiles.Clear();
+            m_CurrentTilePos = m_Grid.WorldToCell(transform.position) + m_FacingDirection;
+            for (int i = 0; i < m_TileMaps.Count; i++)
+            {
+                if (m_TileMaps[i].GetTile(m_CurrentTilePos) != null)
+                {
+                    m_SelectedTiles.Add(m_TileMaps[i].GetTile(m_CurrentTilePos));
+                }
+            }
+
+            if (m_SelectedTiles.Count > 0)
+            {
+                m_TileHighlight.enabled = true;
+                m_CurrentHighlightPos = m_Grid.GetCellCenterWorld(m_CurrentTilePos);
+                m_CurrentHighlightPos.z = HIGHLIGHT_Z_DEPTH;
+                m_TileHighlight.transform.position = m_CurrentHighlightPos;
+            }
+            else
+            {
+                m_TileHighlight.enabled = false;
+            }
+        }
     }
 
     // Handle "X" Button clicked
--- Assets/Scripts/GameFlow.cs
+++ Assets/Scripts/GameFlow.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class GameFlow : MonoBehaviour
 {
+    public static bool s_IsPaused = false;
     [SerializeField] private UIHandler m_UIHandler;
+
+    [Header("Gameplay Elements")]
+    [SerializeField] private CharacterController m_CharacterController;
 
     void Start()
     {
@@ -14,23 +18,32 @@ public class GameFlow : MonoBehaviour
 
     public void PlayGame(bool resumed = false)
     {
+        // initialize gameplay elements
+        if (!resumed)
+        {
+            m_CharacterController.Initialize();
+        }
+        s_IsPaused = false;
         m_UIHandler.ToggleMenuUI(false);
     }
 
     public void PauseGame()
     {
+        s_IsPaused = true;
         m_UIHandler.ToggleMenuUI(true);
         m_UIHandler.ShowMenu(GameConstants.PAUSE_MENU_SCENE_NAME);
     }
 
     public void ShowMainMenu()
     {
+        s_IsPaused = true;
         m_UIHandler.ToggleMenuUI(true);
         m_UIHandler.ShowMenu(GameConstants.MAIN_MENU_SCENE_NAME);
     }
 
     public void ShowGameOver()
     {
+        s_IsPaused = true;
         m_UIHandler.ToggleMenuUI(true);
         m_UIHandler.ShowMenu(GameConstants.GAME_OVER_MENU_SCENE_NAME);
     }
