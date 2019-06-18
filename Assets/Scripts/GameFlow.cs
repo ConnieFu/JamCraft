2e0cbe6cf4c4d63b7336849be2c9f335e157000d
TofuTart <connie.fu.888@gmail.com>
2019-06-17 23:31:08 -0400

    Revert "Revert "More stuff with plants""
    This reverts commit 473d4e67058b1a04fc3b02373588bc5c9014c4e2.

--- Assets/Scripts/GameFlow.cs
+++ Assets/Scripts/GameFlow.cs
@@ -5,15 +5,118 @@ using UnityEngine;
 public class GameFlow : MonoBehaviour
 {
     [SerializeField] private UIHandler m_UIHandler;
+<<<<<<< Updated upstream
 
     void Start()
+=======
+    [SerializeField] private Canvas m_Canvas;
+
+    [Header("Gameplay Elements")]
+    [SerializeField] private CharacterController m_CharacterController;
+    [SerializeField] private Grid m_Grid;
+    [SerializeField] private InteractableTilemap m_InteractableTilemap;
+    [SerializeField] private CraftingManager m_CraftingManager;
+
+    [SerializeField] private Transform m_FloatingResourcesParent;
+
+    public delegate void OnCraftingMenuClosed();
+    private OnCraftingMenuClosed m_CraftingMenuClosed;
+
+
+    private bool m_IsPaused = false;
+    private bool m_IsCrafting = false;
+
+    #region properties
+    public static GameFlow Instance
+    {
+        get
+        {
+            return m_Instance;
+        }
+    }
+
+    public bool IsPaused
+    {
+        get
+        {
+            return m_IsPaused;
+        }
+    }
+
+    public Grid Grid
+    {
+        get
+        {
+            return m_Grid;
+        }
+    }
+
+    public InteractableTilemap InteractableTilemap
+    {
+        get
+        {
+            return m_InteractableTilemap;
+        }
+    }
+
+    public Transform FloatingResourcesParent
+    {
+        get
+        {
+            return m_FloatingResourcesParent;
+        }
+    }
+
+    public bool IsCrafting
+    {
+        get
+        {
+            return m_IsCrafting;
+        }
+    }
+
+    public Canvas Canvas
+    {
+        get
+        {
+            return m_Canvas;
+        }
+    }
+
+    public OnCraftingMenuClosed CraftingMenuClosed
+    {
+        get
+        {
+            return m_CraftingMenuClosed;
+        }
+        set
+        {
+            m_CraftingMenuClosed = value;
+        }
+    }
+    #endregion
+
+    void Awake()
+>>>>>>> Stashed changes
     {
         // show main menu first
         ShowMainMenu();
     }
 
     public void PlayGame(bool resumed = false)
+<<<<<<< Updated upstream
     {
+=======
+    {
+        // initialize gameplay elements
+        if (!resumed)
+        {
+            m_CharacterController.Initialize();
+            m_InteractableTilemap.Initialize();
+            m_CraftingManager.Initialize();
+        }
+        m_IsPaused = false;
+>>>>>>> Stashed changes
         m_UIHandler.ToggleMenuUI(false);
     }
 
@@ -39,4 +142,25 @@ public class GameFlow : MonoBehaviour
     {
         Application.Quit();
     }
+
+    #region Crafting
+    public void ShowCraftingMenu()
+    {
+        m_IsPaused = true;
+        m_IsCrafting = true;
+        m_UIHandler.ShowMenu(GameConstants.CRAFTING_MENU_NAME);
+    }
+
+    public void HideCraftingMenu()
+    {
+        m_IsPaused = false;
+        m_IsCrafting = false;
+        m_UIHandler.HideMenu(GameConstants.CRAFTING_MENU_NAME);
+
+        if (m_CraftingMenuClosed != null)
+        {
+            m_CraftingMenuClosed();
+        }
+    }
+    #endregion
 }
